35c9761844ec9dbe9da24349ebe950e6449c0f80
Mahdy-gribkov <101642367+Mahdy-gribkov@users.noreply.github.com>
2022-07-18 15:01:57 +0300

    Add files via upload

---
+++ ticket.js
@@ -0,0 +1,15 @@
+function ticketPrice(value){
+    var value = document.querySelector('input[name="Age"]:checked').value; 
+    alert("The Ticket price is " + value)
+}
+
+//
+//async function displayRadioValue() {
+    //var ele = document.getElementsByName('age');
+
+   // for(i = 0; i < ele.length; i++) {
+        //if(ele[i].checked)
+            //    return(ele[i].value);
+   // }
+//}
+//
